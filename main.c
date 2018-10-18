78da74868867e19667552a21fd1a2a8dbb16e62a
TJ <tjlee871@gmail.com>
2018-10-18 18:17:18 -0400

    comment update

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main()
   {
     SYSCTL->RCGCGPIO = 1<<5;    //GPIO Port F RMCGC
     SYSCTL->GPIOHBCTL = 1<<5;   //AHB enable
-    GPIOF_AHB->DIR = 0xE;
-    GPIOF_AHB->AFSEL = 0x0;
-    GPIOF_AHB->DEN = 0xE;
+    GPIOF_AHB->DIR = 0xE;       //Set pin as output
+    GPIOF_AHB->AFSEL = 0x0;     //AF select 0
+    GPIOF_AHB->DEN = 0xE;       //Set pin as digital
     GPIOF_AHB->DATA = 0<<1;
     GPIOF_AHB->DATA = 1<<1;
     GPIOF_AHB->DATA = 0<<2;
