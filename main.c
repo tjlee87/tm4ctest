499a2250a5af3e6b3c46b1efecc1ee236c5670f5
TJ <tjlee871@gmail.com>
2018-10-19 14:05:57 -0400

    led change

--- main.c
+++ main.c
@@ -1,19 +1,37 @@
 #include "LM4F120H5QR.h"
 
+void LED_init();
+void SW_init();
+
 int main()
 {
+  SYSCTL->RCGCGPIO |= 1<<5;    //GPIO Port F RMCGC
+  SYSCTL->GPIOHBCTL |= 1<<5;   //AHB enable
+  LED_init();
+  SW_init();
+  //GPIOF_AHB->DATA &= ~(1<<2);
+  //GPIOF_AHB->DATA |= 1<<2;
   while(1)
   {
-    SYSCTL->RCGCGPIO = 1<<5;    //GPIO Port F RMCGC
-    SYSCTL->GPIOHBCTL = 1<<5;   //AHB enable
-    GPIOF_AHB->DIR = 0xE;       //Set pin as output
-    GPIOF_AHB->AFSEL = 0x0;     //AF select 0
-    GPIOF_AHB->DEN = 0xE;       //Set pin as digital
-    GPIOF_AHB->DATA = 0<<1;     //red
-    GPIOF_AHB->DATA = 1<<1;
-    GPIOF_AHB->DATA = 0<<2;     //blue
-    GPIOF_AHB->DATA = 1<<2;
-    GPIOF_AHB->DATA = 0<<3;     //green
-    GPIOF_AHB->DATA = 1<<3;
+    GPIOF_AHB->DATA |= 1<<1;
+    GPIOF_AHB->DATA &= ~(1<<1);     //red
+    GPIOF_AHB->DATA |= 1<<2;
+    GPIOF_AHB->DATA &= ~(1<<2);     //blue
+    GPIOF_AHB->DATA |= 1<<3;
+    GPIOF_AHB->DATA &= ~(1<<3);     //green
   }
+}
+
+void LED_init()
+{
+    GPIOF_AHB->DIR |= 0xE;       //Set pin as output    
+    //GPIOF->AFSEL = 0x0;     //AF select 0
+    GPIOF_AHB->DEN |= 0xE;
+}
+
+void SW_init()
+{
+    GPIOF_AHB->DIR &= ~(0x11);       //Set pin as output
+    //GPIOF_AHB->AFSEL = 0x0;     //AF select 0
+    GPIOF_AHB->DEN |= 0x11;       //Set pin as digital
 }
